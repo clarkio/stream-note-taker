92909cb587b9885043f8a79fba9168201a2411d0
clarkio <clarkio@users.noreply.github.com>
2019-05-30 00:24:26 -0400

    trying this out for stream status

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
+require('dotenv').config();
+const TwitchClient = require('twitch').default;
+const WebHookListener = require('twitch-webhooks').default;
+
 const eventsListener = require('./events-listener');
 const files = require('./files');
 const streamStatus = require('./stream-status');
 
-files.initTodaysStreamNotes();
+const twitchClient = TwitchClient.withClientCredentials(
+  process.env.TWITCH_CLIENT_ID,
+  process.env.TWITCH_CLIENT_SECRET
+);
+
+let streamStatusSubscription;
+WebHookListener.create(twitchClient, { port: 8090 })
+  .then(listener => {
+    listener.listen();
+    listener
+      .subscribeToStreamChanges(process.env.TWITCH_USER_ID, handleStreamChanges)
+      .then(subscription => {
+        streamStatusSubscription = subscription;
+      })
+      .catch(error => console.error(error));
+  })
+  .catch(error => console.error(error));
 
 eventsListener.start();
 
 eventsListener._testEvent('follow');
 
 // console.dir(eventsListener.getSessionData());
+
+function handleStreamChanges(stream) {
+  if (!stream) {
+    console.log('Stream is offline');
+    streamStatusSubscription.stop();
+  }
+}
